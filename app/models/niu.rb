14cc4ff8c3e0959fcd7c29027b299240e1321743
cristianzaninovic <czaninovic@gmail.com>
2023-08-29 16:40:53 -0400

    perms by roles applied on nius
    - site layout updated
    - bootstrap styes applied
    
    Signed-off-by: cristianzaninovic <czaninovic@gmail.com>

--- app/models/niu.rb
+++ app/models/niu.rb
@@ -1,3 +1,5 @@
 class Niu < ApplicationRecord
   belongs_to :nius_user
+  accepts_nested_attributes_for :nius_user, reject_if: ->(attributes){ attributes['content'].blank? }, allow_destroy: true
+
 end
