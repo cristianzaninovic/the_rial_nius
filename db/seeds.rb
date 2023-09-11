29b2783386c9a383b9a602ba1cfe56719a6c1236
cristianzaninovic <czaninovic@gmail.com>
2023-09-10 22:18:54 -0300

    db seedin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do 
+  user = NiusUser.new(
+    :email                 => Faker::Internet.email,
+    :password              => "123456",
+    :password_confirmation => "123456"
+  )
+  user.save!
+end
+
+user = NiusUser.new(
+  :email                 => 'czaninovic@gmail.com',
+  :password              => "123456",
+  :password_confirmation => "123456",
+  :role => 'admin'
+)
+user.save!
+
+40.times do
+  n = Niu.new
+  n.nius_user_id = rand(1..10)
+  n.title = Faker::Lorem.sentence
+  n.headline = Faker::Lorem.sentence
+  n.content = Faker::Lorem.paragraph
+
+  n.save!
+end
+
+puts 'Se han creado 40 noticias aleatorias.'
+
+100.times do
+  comment = Comment.new
+  comment.content = Faker::Lorem.paragraph
+  comment.niu_id = rand(1..40)
+  comment.nius_user_id = rand(1..10)
+
+  comment.save!
+end
+
+puts 'Se han creado 100 comentarios aleatorios.'
